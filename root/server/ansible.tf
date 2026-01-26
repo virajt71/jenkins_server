9de66f8d1ab8fabd5cc3c70122a2d054e00df9cd
virajt71 <soulr27@gmail.com>
2026-01-27 00:19:14 +0100

    terragrunt pipeline

--- root/server/ansible.tf
+++ root/server/ansible.tf
@@ -4,34 +4,6 @@ resource "time_sleep" "wait_30_seconds" {
   create_duration = "30s"
 }
 
-# install terraform with ansible
-module "terraform" {
-  source = "../../modules/ansible"
-
-  playbook_filename = local.terraform
-  host_public_ip    = module.network.public_ip_addresses.web
-
-  vault_pass     = null
-  location_files = null
-
-  extra = {
-    default = {
-      name               = local.admin_username
-      password           = module.compute.generated_passwords.web
-      python_interpreter = "/usr/bin/python3"
-      connection         = "ssh"
-      become_password    = module.compute.generated_passwords.web
-      ssh_common_args    = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PubkeyAuthentication=no"
-      become_password    = module.compute.generated_passwords.web
-    }
-  }
-
-  depends_on = [
-    time_sleep.wait_30_seconds,
-    local.terraform
-  ]
-}
-
 # install docker with ansible
 module "docker" {
   source = "../../modules/ansible"
@@ -54,7 +26,57 @@ module "docker" {
     }
   }
 
-  depends_on = [ module.terraform ]
+  depends_on = [ time_sleep.wait_30_seconds ]
+}
+
+# install terraform with ansible
+module "terraform" {
+  source = "../../modules/ansible"
+
+  playbook_filename = local.terraform
+  host_public_ip    = module.network.public_ip_addresses.web
+
+  vault_pass     = null
+  location_files = null
+
+  extra = {
+    default = {
+      name               = local.admin_username
+      password           = module.compute.generated_passwords.web
+      python_interpreter = "/usr/bin/python3"
+      connection         = "ssh"
+      become_password    = module.compute.generated_passwords.web
+      ssh_common_args    = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PubkeyAuthentication=no"
+      become_password    = module.compute.generated_passwords.web
+    }
+  }
+
+  depends_on = [ local.docker ]
+}
+
+# install terragrunt with ansible
+module "terragrunt" {
+  source = "../../modules/ansible"
+
+  playbook_filename = local.terragrunt
+  host_public_ip    = module.network.public_ip_addresses.web
+
+  vault_pass     = null
+  location_files = null
+
+  extra = {
+    default = {
+      name               = local.admin_username
+      password           = module.compute.generated_passwords.web
+      python_interpreter = "/usr/bin/python3"
+      connection         = "ssh"
+      become_password    = module.compute.generated_passwords.web
+      ssh_common_args    = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PubkeyAuthentication=no"
+      become_password    = module.compute.generated_passwords.web
+    }
+  }
+
+  depends_on = [ local.terraform ]
 }
 
 # install jenkins with ansible
@@ -76,7 +98,7 @@ module "install_jenkins" {
     }
   }
 
-  depends_on = [ module.docker ]
+  depends_on = [ module.terragrunt ]
 }
 
 # configure jenkins variable via ansible
@@ -190,4 +212,48 @@ module "entra_scm_trigger" {
   }
 
   depends_on = [ module.jenkins_pipeline_entra_scm ]
+}
+
+# configure pipeline in jenkins via ansible
+module "jenkins_pipeline_terragrunt_scm" {
+  source = "../../modules/ansible"
+
+  playbook_filename = local.jenkins_pipeline_terragrunt_scm
+  host_public_ip    = module.network.public_ip_addresses.web
+
+  extra = {
+    default = {
+      name               = local.admin_username
+      password           = module.compute.generated_passwords.web
+      python_interpreter = "/usr/bin/python3"
+      connection         = "ssh"
+      become_password    = module.compute.generated_passwords.web
+      ssh_common_args    = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PubkeyAuthentication=no"
+      become_password    = module.compute.generated_passwords.web
+    }
+  }
+
+  depends_on = [ module.jenkins_variables ]
+}
+
+# Trigger  pipeline in jenkins via ansible
+module "terragrunt_scm_trigger" {
+  source = "../../modules/ansible"
+
+  playbook_filename = local.terragrunt_scm_trigger
+  host_public_ip    = module.network.public_ip_addresses.web
+
+  extra = {
+    default = {
+      name               = local.admin_username
+      password           = module.compute.generated_passwords.web
+      python_interpreter = "/usr/bin/python3"
+      connection         = "ssh"
+      become_password    = module.compute.generated_passwords.web
+      ssh_common_args    = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o PubkeyAuthentication=no"
+      become_password    = module.compute.generated_passwords.web
+    }
+  }
+
+  depends_on = [ module.jenkins_pipeline_terragrunt_scm ]
 }
--- root/server/locals.tf
+++ root/server/locals.tf
@@ -8,22 +8,28 @@ locals {
     managed_by  = "terraform"
   }
 
-  terraform        = "../ansible-config/terraform/install_terraform.yml"
-  docker           = "../ansible-config/docker/docker.yml"
-
+  # install
+  docker           = "${path.root}/../ansible-config/docker/docker.yml"
+  terraform        = "${path.root}/../ansible-config/terraform/install_terraform.yml"
+  terragrunt       = "${path.root}/../ansible-config/terraform/install_terragrunt.yml"
   install_jenkins  = "${path.root}/../ansible-config/jenkins/install_jenkins.yml"
-  jenkins_variables= "${path.root}/../ansible-config/jenkins/variables.yml"
+  
+  # jenkins variables
+  jenkins_variables = "${path.root}/../ansible-config/jenkins/variables.yml"
 
-  jenkins_pipeline_entra = "${path.root}/../ansible-config/jenkins/pipeline_entra.yml"
-  jenkins_pipeline_entra_scm = "${path.root}/../ansible-config/jenkins/pipeline_entra_scm.yml"
+  # pipeline
+  jenkins_pipeline_entra          = "${path.root}/../ansible-config/jenkins/pipeline_entra.yml"
+  jenkins_pipeline_entra_scm      = "${path.root}/../ansible-config/jenkins/pipeline_entra_scm.yml"
+  jenkins_pipeline_terragrunt_scm = "${path.root}/../ansible-config/jenkins/pipeline_entra_scm.yml"
 
-  entra_trigger = "${path.root}/../ansible-config/jenkins/entra_trigger.yml"
-  entra_scm_trigger = "${path.root}/../ansible-config/jenkins/entra_scm_trigger.yml"
-
+  # trigger
+  entra_trigger          = "${path.root}/../ansible-config/jenkins/trigger/entra_trigger.yml"
+  entra_scm_trigger      = "${path.root}/../ansible-config/jenkins/trigger/entra_scm_trigger.yml"
+  terragrunt_scm_trigger = "${path.root}/../ansible-config/jenkins/trigger/terragrunt_scm_trigger.yml"
 
   # Vault configuration
-  vault_file          = "../ansible-config/jenkins/vault/secrets.yml"
-  vault_password_file = "../ansible-config/jenkins/vault-pass.txt"
+  vault_file          = "${path.root}/../ansible-config/jenkins/vault/secrets.yml"
+  vault_password_file = "${path.root}/../ansible-config/jenkins/vault-pass.txt"
 
   # Dynamic path for ANSIBLE_VAULT_PASSWORD_FILE (project-relative; works from any clone location)
   ansible_vault_password_file_path = abspath("${path.root}/../ansible-config/jenkins/vault-pass.txt")
